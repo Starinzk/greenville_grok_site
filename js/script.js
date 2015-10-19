c2be710482c3d64339e70f6dce95d186eddf2fda
Starinzk <zachary.starin@gmail.com>
2015-10-19 09:09:58 -0400

    updated files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-/* js file */
-
 (function( window, undefined ){
 
+    
+	var divName = $( ".page" );
+    var buttons = $( ".nav_bar" ).find( "a" ); 
+
+
+
+    buttons.on("click", function (e){
+
+    	var className = $( this ).data( "page");
+
+    	divName.hide();
+    	$( className ).fadeIn(1000);
+    	e.preventDefault();
+
+    });
+
+    divName.hide();
+    $(".p1").fadeIn( 1000 );
+
 })( window, undefined );
+
+ 
